18eb7658e99136f2772a210c962b66d3602f7117
rossputin <rossajmcd@gmail.com>
2018-04-24 15:53:24 +0100

    a better representation of FSM with more transitional capability

--- test/dahlplayground/core_test.clj
+++ test/dahlplayground/core_test.clj
@@ -12,8 +12,16 @@
    :MakingCoffee {:get-status :MakingCoffee
                   :prepared :MadeCoffee}})
 
+(def better-drink-machine
+  {nil {:init :Ready}
+   :Ready {:make-tea :MakingTea}
+   :MakingTea {:get-status :GettingTeaStatus}
+   :GettingTeaStatus {:making-tea :MakingTea
+                      :completing-tea :MadeTea
+                      :erroring-tea :ErroredTea}})
+
 (deftest blurb
-  (viz/generate-image drink-machine-config
+  (viz/generate-image better-drink-machine
                       "fsm.png")
   (is (= (next-node drink-machine-config nil :init) :Ready))
   (is (= (next-node drink-machine-config :Ready :make-tea) :MakingTea)))
